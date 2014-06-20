99a0754f644bb12cc485d9e2d563cf63738fc9ec
John Cionci <johcionci@gmail.com>
2014-06-20 10:04:24 -0400

    Upstream Sync - bring catergorized_blog() up to date

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -143,27 +143,32 @@ function braces_posted_on() {
 }
 
 /**
- * Returns true if a blog has more than 1 category
+ * Returns true if a blog has more than 1 category.
+ *
+ * @return bool
  */
 function braces_categorized_blog() {
-	if ( false === ( $all_the_cool_cats = get_transient( 'all_the_cool_cats' ) ) ) {
-		// Create an array of all the categories that are attached to posts
+	if ( false === ( $all_the_cool_cats = get_transient( 'braces_categories' ) ) ) {
+		// Create an array of all the categories that are attached to posts.
 		$all_the_cool_cats = get_categories( array(
+			'fields'     => 'ids',
 			'hide_empty' => 1,
+
+			// We only need to know if there is more than one category.
+			'number'     => 2,
 		) );
 
-		// Count the number of categories that are attached to the posts
+		// Count the number of categories that are attached to the posts.
 		$all_the_cool_cats = count( $all_the_cool_cats );
 
-		set_transient( 'all_the_cool_cats', $all_the_cool_cats );
+		set_transient( 'braces_categories', $all_the_cool_cats );
 	}
 
-	if ( '1' != $all_the_cool_cats ) {
-		// This blog has more than 1 category so braces_categorized_blog should return true
+	if ( $all_the_cool_cats > 1 ) {
+		// This blog has more than 1 category so braces_categorized_blog should return true.
 		return true;
-	}
-	else {
-		// This blog has only 1 category so braces_categorized_blog should return false
+	} else {
+		// This blog has only 1 category so braces_categorized_blog should return false.
 		return false;
 	}
 }
