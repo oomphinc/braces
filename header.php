426106b1c7ea502e8c3f17c8af6a4cca7940e455
John Cionci <johcionci@gmail.com>
2014-06-20 10:50:55 -0400

    Upstream Sync - move skip-to-content link to top to make it first focusable element

--- header.php
+++ header.php
@@ -27,6 +27,9 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
+
+		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'braces' ); ?></a>
+
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
@@ -34,8 +37,6 @@
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle"><?php _e( 'Primary Menu', 'braces' ); ?></button>
-			<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'braces' ); ?>"><?php _e( 'Skip to content', 'braces' ); ?></a></div>
-
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
