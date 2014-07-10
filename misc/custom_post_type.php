a990513aee76fae69cfdc514da3f3c29da86ff8c
Jim Reevior <jim@oomphinc.com>
2014-07-10 17:25:17 -0400

    Modified CPT to use constants and include revisions

--- misc/custom_post_type.php
+++ misc/custom_post_type.php
@@ -10,6 +10,12 @@
  */
 
 class Braces_Custom_Post_Type {
+	// Post Types
+	const POST_TYPE_SLUG     = 'braces';
+	const POST_TYPE_NAME     = 'Braces';
+	const POST_TYPE_SINGULAR = 'Braces';
+	const POST_TYPE_CAP      = 'post';
+	const POST_TYPE_SUPPORTS = array( 'title', 'page-attributes', 'editor', 'revisions', 'thumbnail' );
 
 	function __construct() {
 		add_action( 'init', array( $this, 'action_init' ) );
@@ -21,18 +27,18 @@ class Braces_Custom_Post_Type {
 	 */
 	function action_init() {
 		$labels = array(
-			'name'               => _x( 'Post Types', 'post type general name', 'braces' ),
-			'singular_name'      => _x( 'Post Type', 'post type singular name', 'braces' ),
-			'menu_name'          => _x( 'Post Types', 'admin menu', 'braces' ),
-			'name_admin_bar'     => _x( 'Post Type', 'add new on admin bar', 'braces' ),
-			'add_new'            => _x( 'Add New', 'post type', 'braces' ),
-			'add_new_item'       => __( 'Add New Post Type', 'braces' ),
-			'new_item'           => __( 'New Post Type', 'braces' ),
-			'edit_item'          => __( 'Edit Post Type', 'braces' ),
-			'view_item'          => __( 'View Post Type', 'braces' ),
-			'all_items'          => __( 'All Post Types', 'braces' ),
-			'search_items'       => __( 'Search Post Types', 'braces' ),
-			'parent_item_colon'  => __( 'Parent Post Types:', 'braces' ),
+			'name'               => _x( self::POST_TYPE_NAME, 'post type general name', 'braces' ),
+			'singular_name'      => _x( self::POST_TYPE_SINGULAR, 'post type singular name', 'braces' ),
+			'menu_name'          => _x( self::POST_TYPE_NAME, 'admin menu', 'braces' ),
+			'name_admin_bar'     => _x( self::POST_TYPE_SINGULAR, 'add new on admin bar', 'braces' ),
+			'add_new'            => _x( 'Add New '. self::POST_TYPE_SINGULAR, 'post type', 'braces' ),
+			'add_new_item'       => __( 'Add New '. self::POST_TYPE_SINGULAR, 'braces' ),
+			'new_item'           => __( 'New '. self::POST_TYPE_SINGULAR, 'braces' ),
+			'edit_item'          => __( 'Edit '. self::POST_TYPE_SINGULAR, 'braces' ),
+			'view_item'          => __( 'View '. self::POST_TYPE_SINGULAR, 'braces' ),
+			'all_items'          => __( 'All '. self::POST_TYPE_NAME, 'braces' ),
+			'search_items'       => __( 'Search '. self::POST_TYPE_SINGULAR, 'braces' ),
+			'parent_item_colon'  => __( 'Parent '. self::POST_TYPE_SINGULAR. ':', 'braces' ),
 			'not_found'          => __( 'No post types found.', 'braces' ),
 			'not_found_in_trash' => __( 'No post types found in Trash.', 'braces' ),
 		);
@@ -45,19 +51,19 @@ class Braces_Custom_Post_Type {
 			'show_in_menu'       => true,
 			'query_var'          => true,
 			'rewrite'            => true,
-			'capability_type'    => 'post',
+			'capability_type'    => self::POST_TYPE_CAP,
 			'has_archive'        => true,
 			'hierarchical'       => false,
 			'menu_position'      => 20,
-			'supports'           => array( 'title', 'page-attributes', 'editor', 'thumbnail' ),
-			'rewrite'            => array ( 'slug' => 'post_type', 'with_front' => false ),
+			'supports'           => self::POST_TYPE_SUPPORTS,
+			'rewrite'            => array ( 'slug' => self::POST_TYPE_SLUG, 'with_front' => false ),
 			'taxonomies'         => array (
 				0 => 'category',
 				1 => 'post_tag',
 			),
 		);
 
-		register_post_type('post_type', $args);
+		register_post_type(self::POST_TYPE_SLUG, $args);
 	}
 }
 
