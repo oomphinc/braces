edc812fce8aabc75598966edca09ca5f4252ec8d
John Cionci <johcionci@gmail.com>
2015-04-07 09:30:14 -0400

    OBT - adding some output to style template

--- theme/page-styleguide.php
+++ theme/page-styleguide.php
@@ -25,14 +25,47 @@ get_header(); ?>
 
 			<section class="styleguide-section">
 				<h2>Colors</h2>
+				<?php for ($i=0; $i < 4; $i++) { ?>
+					<div class="color-block">
+						<div class="color">Color</div>
+						<div class="color-shade">Color Shade</div>
+						<div class="color-shade">Color Shade</div>
+						<div class="color-shade">Color Shade</div>
+						<div class="color-shade">Color Shade</div>
+					</div>
+				<?php } ?>
 			</section>
 
 			<section class="styleguide-section">
 				<h2>Typography</h2>
+				<h1>h1</h1>
+				<h2>h2</h2>
+				<h3>h3</h3>
+				<h4>h4</h4>
+				<h5>h5</h5>
+				<h6>h6</h6>
+				<p>Pellentesque habitant morbi <strong>tristique senectus et netus et</strong> malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+				<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
+				<ul>
+					<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+					<li>Aliquam tincidunt mauris eu risus.</li>
+				</ul>
+				<ol>
+					<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+					<li>Aliquam tincidunt mauris eu risus.</li>
+				</ol>
 			</section>
 
 			<section class="styleguide-section">
 				<h2>Pagination</h2>
+				<h3>Post Navigation</h3>
+				<nav class="navigation paging-navigation" role="navigation">
+					<h1 class="screen-reader-text sr-only sr-only-focusable">Post navigation</h1>
+					<ul class="nav-links pager list-unstyled">
+						<li class="nav-previous previous"><a href="#" class="prev-post"><span class="meta-nav" aria-hidden="true">&larr;</span> Older posts</a></li>
+						<li class="nav-next next"><a href="#" class="next-post">Newer posts <span class="meta-nav" aria-hidden="true">&rarr;</span></a></li>
+					</ul><!-- .nav-links -->
+				</nav>
 			</section>
 
 			<section class="styleguide-section">
@@ -41,14 +74,11 @@ get_header(); ?>
 
 			<section class="styleguide-section">
 				<h2>Post Meta</h2>
+				<?php braces_posted_on(); ?>
 			</section>
 
 			<section class="styleguide-section">
 				<h2>Comments</h2>
-			</section>
-
-			<section class="styleguide-section">
-				<h2>Forms</h2>
 			</section>
 
 			<section class="styleguide-section">
