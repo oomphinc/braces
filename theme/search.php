66cdd5450eef23dd9a85ca6ca38186da2758f69e
John Cionci <john@thinkoomph.com>
2015-05-20 21:25:38 -0400

    - updateing escaping functions

--- theme/search.php
+++ theme/search.php
@@ -17,7 +17,7 @@ get_header(); ?>
 			if ( have_posts() ) { ?>
 
 				<header class="page-header">
-					<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'braces' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+					<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'braces' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<?php
